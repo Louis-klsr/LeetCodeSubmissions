1cd9a85b14d9f4cbddaf5b9528ce39f2bd8cbdde
Louis-klsr <23pa1a4579@vishnu.edu.in>
2026-08-21 23:26:08 +0530

    268. Missing Number

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
+
+        for (int i = 0; i < nums.length ; i++ ) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
