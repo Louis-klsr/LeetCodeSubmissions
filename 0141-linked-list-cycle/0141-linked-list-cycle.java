2eb180940af8fe1995a2df6a5d14d54def7685fd
Louis-klsr <23pa1a4579@vishnu.edu.in>
2026-08-21 23:19:45 +0530

    141. Linked List Cycle

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode f = head;
+        ListNode s = head;
+        while (f != null && f.next != null ){
+            s = s.next;
+            f = f.next.next;
+            if (s == f) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
