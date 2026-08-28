bc5c36b66352f6a8af4ca1c3f0a429a930421f48
Louis-klsr <23pa1a4579@vishnu.edu.in>
2026-08-28 21:05:53 +0530

    61. Rotate List

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0){
+            return head;
+        }
+        int len = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            len++;
+        }
+        k = k % len;
+        if (k == 0) {
+            return head;
+        }
+        tail.next = head;
+        int steps = len - k;
+        ListNode newtail = head;
+        for (int i = 1; i < steps; i++) {
+            newtail = newtail.next;
+        }
+        ListNode newhead = newtail.next;
+        newtail.next = null;
+        return newhead;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
